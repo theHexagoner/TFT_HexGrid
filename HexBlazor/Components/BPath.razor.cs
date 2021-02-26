2037ebf046dc1becd4950bb9078e9bdf13f695e0
Gerald Moore <jurld@comcast.net>
2021-02-25 19:48:21 -0500

    Add project files.

---
+++ HexBlazor/Components/BPath.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace HexBlazor.Components
+{
+    public partial class BPath : ComponentBase
+    {
+        // e.g. <path d="M20,230 Q40,205 50,230 T90,230" fill="none" stroke="blue" stroke-width="5"/>
+
+        [Parameter]
+        public int Id { get; set; }
+
+        [Parameter]
+        public string D { get; set; }
+
+        [CascadingParameter]
+        public BSvg Container { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Container.AddBPath(this);
+        }
+
+        public async Task SetD(string d)
+        {
+            D = d;
+            StateHasChanged();
+        }
+
+    }
+}
---
+++ HexBlazor/TFT_HexGrid/Hexagon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TFT_HexGrid.Grids
+{
+    public class Hexagon
+    {
+        // no default constructor
+        private Hexagon() { }
+
+        /// <summary>
+        /// construct a Hexagon by passing the grid and offset coordinates
+        /// </summary>
+        /// <param name="grid">the parent grid that contains this hexagon</param>
+        /// <param name="coords">the offset coordinates of the hexagon</param>
+        public Hexagon(Grid grid, Offset coords) : this(grid, Cube.GetCubeFromOffset(grid.OffsetScheme, coords), coords) { }
+
+        /// <summary>
+        /// construct a hexagon by passing the grid and cubic coordinates
+        /// </summary>
+        /// <param name="grid">the parent grid that contains this hex</param>
+        /// <param name="coords">the cubic coordinates of the hexagon</param>
+        public Hexagon(Grid grid, Cube coords) : this(grid, coords, Offset.GetOffset(grid.OffsetScheme, coords)) {}
+
+        private Hexagon(Grid grid, Cube cubeCoords, Offset offsetCoords)
+        {
+            OffsetLocation = offsetCoords;
+            CubicLocation = cubeCoords;
+            ID = grid.GetHashcodeForCube(cubeCoords);
+            Points = grid.GetHexCornerPoints(cubeCoords);
+        }
+
+        public int ID { get; private set; }
+
+        /// <summary>
+        /// cubic coordinates of the hexagon within the grid
+        /// X, Y and Z dimensions to represent 2d grid as 3d matrix
+        /// used to calculate most things like distance to other hexes, line of sight, etc.
+        /// </summary>        
+        public Cube CubicLocation { get; private set; }
+
+        /// <summary>
+        /// offset (row and column) coordinates of the hexagon within the grid
+        /// </summary>
+        public Offset OffsetLocation { get; private set; }
+
+        /// <summary>
+        /// the points that define the six corners of the hexagon
+        /// </summary>
+        public GridPoint[] Points { get; private set; }
+
+    }
+
+}
